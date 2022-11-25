8120b94d294c058c8f09c84bcf3470966c41c145
KimJaehyun <rlawwogus@ajou.ac.kr>
2022-11-26 00:04:38 +0900

    regular expression is really important...

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {see, logout, getEdit, postEdit, startGithubLogin, finishGithubLogin, getChanggePassword, postChanggePassword} from "../controllers/userController"
+import {see, logout, getEdit, postEdit, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword} from "../controllers/userController"
 import { uploadAvatar, protectorMiddleware, publicOnlyMiddleware} from "../middlewares";
 const userRouter = express.Router();
 
@@ -7,8 +7,8 @@ userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadAvatar.single("avatar"), postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.get("/:id", see);
-userRouter.route("/change-password").all(protectorMiddleware).get(getChanggePassword).post(postChanggePassword);
+userRouter.get("/:id([0-9a-f]{24})", see);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 
 
 export default userRouter;
